723bb4e0f13bfb5b569d97fc68e228d77ef4e0a8
DigiXArt <108691347+DigiXArt@users.noreply.github.com>
2020-04-15 14:47:52 +0000

    More Stuff

--- services/auth/src/api_errors.go
+++ services/auth/src/api_errors.go
@@ -64,4 +64,4 @@ func RaiseError(w http.ResponseWriter, message string, statusCode int, code Erro
 		Code:       code,
 	}
 
-	log.Printf("Error: %v. HTTP Status: %v Code: %
+	log.Printf("Error: %v. HTTP Status: %v Code: %v", errorMessage.Message, errorMessage.StatusCode,

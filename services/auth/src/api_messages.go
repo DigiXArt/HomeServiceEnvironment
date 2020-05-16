9bb5e2276219783c5961f5f4caff85ccf456b20c
DigiXArt <108691347+DigiXArt@users.noreply.github.com>
2020-05-16 13:37:17 +0000

    Updated composer lock

--- services/auth/src/api_messages.go
+++ services/auth/src/api_messages.go
@@ -1,3 +1,5 @@
 /*
 api_messages.go
-Defines all types that are either served via the API or received
+Defines all types that are either served via the API or received from the client.
+
+############################

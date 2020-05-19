2d3b1aa24ae4784e31d4e3a051b97fc13443c169
DigiXArt <108691347+DigiXArt@users.noreply.github.com>
2020-05-19 09:56:56 +0000

    Fixed coding standards

--- services/auth/src/api_messages.go
+++ services/auth/src/api_messages.go
@@ -2,4 +2,4 @@
 api_messages.go
 Defines all types that are either served via the API or received from the client.
 
-############################
+###############################################################################

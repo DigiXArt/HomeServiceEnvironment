4aaef6ef6c03f57169ca0e62a681ff21f8d1a078
DigiXArt <108691347+DigiXArt@users.noreply.github.com>
2020-05-19 14:41:29 +0000

    link to blog

--- services/auth/src/api_messages.go
+++ services/auth/src/api_messages.go
@@ -2,4 +2,8 @@
 api_messages.go
 Defines all types that are either served via the API or received from the client.
 
-###############################################################################
+###################################################################################
+
+MIT License
+
+Copyright (c) 2020 Bruno

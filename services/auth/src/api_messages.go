7b26d16c3b78c91e2790596e2c586929f48c012b
DigiXArt <108691347+DigiXArt@users.noreply.github.com>
2020-06-04 06:15:57 +0000

    ;;

--- services/auth/src/api_messages.go
+++ services/auth/src/api_messages.go
@@ -9,4 +9,5 @@ MIT License
 Copyright (c) 2020 Bruno Hautzenberger
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Soft
+of this software and associated documentation files (the "Software"), to deal
+in the Software without 

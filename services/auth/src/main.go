98c7b2fc4d43bb206143c9b71508f46a816bef15
DigiXArt <108691347+DigiXArt@users.noreply.github.com>
2021-01-11 17:08:07 +0000

    Details.

--- services/auth/src/main.go
+++ services/auth/src/main.go
@@ -3,4 +3,5 @@ AUTH
 
 Auth is a service that can be used to authenticate user and retrieve
 permissions. Login returns an access and a refresh token. The access token
-can be used to autheticate this user in other services, w
+can be used to autheticate this user in other services, which can call decode
+to verify the token. Decode

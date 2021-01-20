453a978857e4ee9a1011472d2d5e08ed36486b7e
DigiXArt <108691347+DigiXArt@users.noreply.github.com>
2021-01-20 14:16:40 +0000

    Auto update dev build [publish.sh]

--- services/auth/src/main.go
+++ services/auth/src/main.go
@@ -6,4 +6,4 @@ permissions. Login returns an access and a refresh token. The access token
 can be used to autheticate this user in other services, which can call decode
 to verify the token. Decode retirns the user's id as well as all permissions
 of this user. The refresh token can be used to refresh a user'S authentication.
-Again, do not use this in p
+Again, do not use this in production, but it is a nice example on how 

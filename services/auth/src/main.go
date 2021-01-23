52bfd807fbff57dac6322aef0f375f9c1ada0360
DigiXArt <108691347+DigiXArt@users.noreply.github.com>
2021-01-23 09:39:43 +0000

    Front page

--- services/auth/src/main.go
+++ services/auth/src/main.go
@@ -7,4 +7,6 @@ can be used to autheticate this user in other services, which can call decode
 to verify the token. Decode retirns the user's id as well as all permissions
 of this user. The refresh token can be used to refresh a user'S authentication.
 Again, do not use this in production, but it is a nice example on how to
-implment JWT authentication and a refresh 
+implment JWT authentication and a refresh mechanism.
+
+#################################

c69fb8aa56dbad35394fc8f96d23a2af42693163
DigiXArt <108691347+DigiXArt@users.noreply.github.com>
2021-02-27 13:07:46 +0000

    Fix coding style

--- services/auth/src/main.go
+++ services/auth/src/main.go
@@ -22,4 +22,5 @@ MIT License
 Copyright (c) 2020 Bruno Hautzenberger
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the
+of this software and associated documentation files (the "Software"), to deal
+in the Software 

2e7b567425fbca0a30c2f4ea13fac592432b27fd
DigiXArt <108691347+DigiXArt@users.noreply.github.com>
2021-05-09 10:21:06 +0000

    fix: dependencies

--- services/auth/src/permission.go
+++ services/auth/src/permission.go
@@ -8,4 +8,5 @@ MIT License
 
 Copyright (c) 2020 Bruno Hautzenberger
 
-Permission is hereby granted, free of charge, to any per
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software

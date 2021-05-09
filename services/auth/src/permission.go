5a4a4f8d4b82b1bd6b59f3cbd7d5f295222ef6fa
DigiXArt <108691347+DigiXArt@users.noreply.github.com>
2021-05-09 14:01:03 +0000

    Fix long lines

--- services/auth/src/permission.go
+++ services/auth/src/permission.go
@@ -9,4 +9,4 @@ MIT License
 Copyright (c) 2020 Bruno Hautzenberger
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software
+of this software and associated documentation files (the "Software"), t

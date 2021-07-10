b5804069ec48e956d7986036e61dadc88c921860
DigiXArt <108691347+DigiXArt@users.noreply.github.com>
2021-07-10 12:57:45 +0000

    style(any): code poem

--- services/auth/src/token_builder.go
+++ services/auth/src/token_builder.go
@@ -22,3 +22,42 @@ copies or substantial portions of the Software.
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+package main
+
+import (
+	"encoding/json"
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+// TokenBuilderInterface defines the interface for token builders
+type TokenBuilderInterface interface {
+	CreateUserToken(user *User) (*UserTokenData, error)
+	CreateServiceToken(service *Service) (*ServiceTokenData, error)
+}
+
+// UserTokenData holds all information about a user token.
+type UserTokenData struct {
+	AccessToken  string
+	RefreshToken string
+	ATExpiresAt  time.Time
+	RFExpiresAt  time.Time
+}
+
+// ServiceTokenData holds all information about a service token.
+type ServiceTokenData struct {
+	AccessToken string
+}
+
+// TokenBuilder implements TokenbuilderInterface
+type TokenBuilder struct{}
+
+// CreateUserToken builds a new UserTokenData instance for given user
+func (t *TokenBuilder) CreateUserToken(user *User) (*UserTokenData, error) {

dbfd0a8c2c5c10c5538f1c1f9184b3fd9de26742
DigiXArt <108691347+DigiXArt@users.noreply.github.com>
2021-07-25 15:02:47 +0000

    Fixed Travis script directive and dropped MRI 1.9.3.

--- services/auth/src/user.go
+++ services/auth/src/user.go
@@ -10,4 +10,4 @@ Copyright (c) 2020 Bruno Hautzenberger
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
-in the S
+in the Software without restriction, including without limitati

7f53b856be5ba5d2375f2ff7d698f66eb3290b0b
DigiXArt <108691347+DigiXArt@users.noreply.github.com>
2021-07-25 05:41:29 +0000

    Update status

--- services/auth/src/user.go
+++ services/auth/src/user.go
@@ -9,4 +9,5 @@ MIT License
 Copyright (c) 2020 Bruno Hautzenberger
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated do
+of this software and associated documentation files (the "Software"), to deal
+in the S

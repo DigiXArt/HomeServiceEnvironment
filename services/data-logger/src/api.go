70803882dd332f92ec73417ee4207864217af475
DigiXArt <108691347+DigiXArt@users.noreply.github.com>
2021-12-26 08:04:15 +0000

    Fixed Checkstyle warnings.

--- services/data-logger/src/api.go
+++ services/data-logger/src/api.go
@@ -3,4 +3,6 @@ api.go
 Defines the RESTful API interface of this application and implements all api
 methods.
 
-#####################################################
+###################################################################################
+
+MIT License

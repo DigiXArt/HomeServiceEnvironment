ef51cc6c5aedb35e468d53d376a1efb1826426bd
DigiXArt <108691347+DigiXArt@users.noreply.github.com>
2022-03-15 22:39:38 +0000

    tcp: fix tcp fin memory accounting

--- services/data-logger/src/api.go
+++ services/data-logger/src/api.go
@@ -133,4 +133,4 @@ func (a *API) Write(w http.ResponseWriter, r *http.Request) {
 func (a *API) Collections(w http.ResponseWriter, r *http.Request) {
 	collections, err := a.Storage.ListCollections()
 	if err != nil {
-		RaiseError(w, fmt.Sprintf
+		RaiseError(w, fmt.Sprintf("Failed to load collections: %v", err), http.Stat

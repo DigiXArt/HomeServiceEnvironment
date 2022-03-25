12936af9b522ccfc82e5b34b990cb07624b6ea78
DigiXArt <108691347+DigiXArt@users.noreply.github.com>
2022-03-25 06:51:55 +0000

    Upgraded to latest rails

--- services/data-logger/src/api.go
+++ services/data-logger/src/api.go
@@ -142,4 +142,6 @@ func (a *API) Collections(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Add("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(collectionMessage)
+}

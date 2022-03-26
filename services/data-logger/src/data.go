565f655e8a4e6662129ad8eede9ac2d688392c8b
DigiXArt <108691347+DigiXArt@users.noreply.github.com>
2022-03-26 21:10:02 +0000

    Bump to v0.5.0

--- services/data-logger/src/data.go
+++ services/data-logger/src/data.go
@@ -1,4 +1,7 @@
 /*
 data.go
 Implements a single data instance, which is saved to a collection storage.
-It also takes care of initializing the Data instance with a
+It also takes care of initializing the Data instance with a UUID and a created-at
+date.
+
+###################

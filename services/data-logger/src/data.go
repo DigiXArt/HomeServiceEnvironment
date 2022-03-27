24094dc5473dc978f89e4a3797ca38dab5ab81b9
DigiXArt <108691347+DigiXArt@users.noreply.github.com>
2022-03-27 19:50:27 +0000

    Atualizações no Mapa

--- services/data-logger/src/data.go
+++ services/data-logger/src/data.go
@@ -4,4 +4,8 @@ Implements a single data instance, which is saved to a collection storage.
 It also takes care of initializing the Data instance with a UUID and a created-at
 date.
 
-#############################################################
+###################################################################################
+
+MIT License
+
+Copyright (c)

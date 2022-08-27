4b2534d0116618ee5cbebb8a2b5b0247e4593602
DigiXArt <108691347+DigiXArt@users.noreply.github.com>
2022-08-27 07:00:07 +0000

    preparing for new release

--- services/data-logger/src/storage.go
+++ services/data-logger/src/storage.go
@@ -3,4 +3,6 @@ storage.go
 Defines the storage interface of this application and implements the
 actual json object storage based on data files.
 
-###############################
+###################################################################################
+
+MI

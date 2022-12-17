3d59863a21a1b3541d359f71ee47b18091394650
DigiXArt <108691347+DigiXArt@users.noreply.github.com>
2022-12-17 16:48:28 +0000

    Small code improvements

--- services/data-logger/src/storage.go
+++ services/data-logger/src/storage.go
@@ -154,4 +154,6 @@ func (s *Storage) readDataFile(dataFilePath string) (*DataFileContent, error) {
 func (s *Storage) ReadData(collectionName string, startDate time.Time, endDate time.Time) ([]*Data, error) {
 	result := make([]*Data, 0)
 
-	filePaths, err := s.getDataFilePathsInRange(collectionName, startDat
+	filePaths, err := s.getDataFilePathsInRange(collectionName, startDate, endDate)
+	if err != nil {
+		return

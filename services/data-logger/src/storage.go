4a371f4c38962db96274dde841f42f190e4545e3
DigiXArt <108691347+DigiXArt@users.noreply.github.com>
2022-12-25 21:32:53 +0000

    chore(package): update eslint to version 3.3.0

--- services/data-logger/src/storage.go
+++ services/data-logger/src/storage.go
@@ -174,4 +174,6 @@ func (s *Storage) ReadData(collectionName string, startDate time.Time, endDate t
 }
 
 // writeDataFile writes a data file
-func (s *Storage) writeDataFile(dataFilePath string, file []byte) e
+func (s *Storage) writeDataFile(dataFilePath string, file []byte) error {
+	s.MutexLock.Lock()
+	defer

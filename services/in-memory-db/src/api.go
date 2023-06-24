d22b8e0cd53f4b68459928bee01340617df11823
DigiXArt <108691347+DigiXArt@users.noreply.github.com>
2023-06-24 16:34:37 +0000

    fix reference

--- services/in-memory-db/src/api.go
+++ services/in-memory-db/src/api.go
@@ -46,3 +46,75 @@ type APIInterface interface {
 	Keys(w http.ResponseWriter, r *http.Request)
 	Realms(w http.ResponseWriter, r *http.Request)
 	Initialize(storage StorageInterface)
+}
+
+//API implements APIInterface
+type API struct {
+	Storage StorageInterface
+}
+
+//Initialize initializes the API by setting the active storage
+func (a *API) Initialize(storage StorageInterface) {
+	a.Storage = storage
+}
+
+//API handler to get values
+func (a *API) Get(w http.ResponseWriter, r *http.Request) {
+	// Get Request Vars
+	vars := mux.Vars(r)
+	realm, ok := vars["realm"]
+	if !ok {
+		RaiseError(w, "Realm is missing", http.StatusBadRequest, ErrorCodeRealmMissing)
+		return
+	}
+
+	key, ok := vars["key"]
+	if !ok {
+		RaiseError(w, "Key is missing", http.StatusBadRequest, ErrorCodeKeyMissing)
+		return
+	}
+
+	// Load value
+	ok, value := a.Storage.Get(realm, key)
+	if !ok {
+		RaiseError(w, fmt.Sprintf("No value found for key %v/%v", realm, key), http.StatusNotFound, ErrorCodeEntityNotFound)
+		return
+	}
+
+	// Write Response
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(value.ToValueMessageType())
+}
+
+//API handler to set values
+func (a *API) Set(w http.ResponseWriter, r *http.Request) {
+	// Get Request Vars
+	vars := mux.Vars(r)
+	realm, ok := vars["realm"]
+	if !ok {
+		RaiseError(w, "Realm is missing", http.StatusBadRequest, ErrorCodeRealmMissing)
+		return
+	}
+
+	key, ok := vars["key"]
+	if !ok {
+		RaiseError(w, "Key is missing", http.StatusBadRequest, ErrorCodeKeyMissing)
+		return
+	}
+
+	value, err := ValueFromValueMessageType(r.Body)
+	if err != nil {
+		RaiseError(w, "Invalid request body", http.StatusBadRequest, ErrorCodeInvalidRequestBody)
+		return
+	}
+
+	a.Storage.Set(realm, key, value)
+
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(value.ToValueMessageType())
+}
+
+//API handler to delete values
+func (a *API) Delete(w http.ResponseWriter, r *http.Request) {

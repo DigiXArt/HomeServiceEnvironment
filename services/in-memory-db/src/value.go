0cc98b5830b1a9d22a2025ac5d0a68f05e964f1e
DigiXArt <108691347+DigiXArt@users.noreply.github.com>
2023-08-25 18:32:09 +0000

    remove unnecessary include

--- services/in-memory-db/src/value.go
+++ services/in-memory-db/src/value.go
@@ -1,4 +1,5 @@
 /*
 value.go
 Implements a single key/value instance, which is saved to the key/value storage.
-It also takes care of always setting the right remaining
+It also takes care of always setting the right remaining expire time every time
+a value is served via the API
